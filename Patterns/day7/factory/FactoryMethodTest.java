cb00fa0833ebfbd7ef4ab9cab253cd1270cbb338
P-Jay-C <azilakujeremiah.gmail.com>
2025-04-30 16:45:54 +0000

    Implement Proxy Pattern with Image, ProxyImage, and RealImage classes

--- Patterns/day7/factory/FactoryMethodTest.java
+++ Patterns/day7/factory/FactoryMethodTest.java
@@ -1,9 +1,7 @@
 package day7.factory;
 
-import javax.print.Doc;
-
 public class FactoryMethodTest {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         // Create a factory for creating books
        DocumentCreator pdfCreator = new PDFDocumentCreator();
        DocumentCreator wordCreator = new WordDocumentCreator();
