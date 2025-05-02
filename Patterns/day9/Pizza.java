21b9ef03dfe222b60bc7b545bd9a7071547f55fe
P-Jay-C <azilakujeremiah.gmail.com>
2025-05-02 09:08:03 +0000

    Implement Builder Pattern with Pizza and PizzaBuilder classes, and add BuilderTest for testing

---
+++ Patterns/day9/Pizza.java
@@ -0,0 +1,45 @@
+package day9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    private String size;
+    private String crust;
+    private List<String> toppings;
+
+    public Pizza(PizzaBuilder pizzaBuilder) {
+        this.size = pizzaBuilder.size;
+        this.crust = pizzaBuilder.crust;
+        this.toppings = pizzaBuilder.toppings;
+    }
+
+    public void display() {
+        System.out.println("Pizza: Size=" + size + ", Crust=" + crust + ", Toppings=" + toppings);
+    }
+
+    public static class PizzaBuilder{
+        private String size;
+        private String crust;
+        private List<String> toppings = new ArrayList<>();
+
+        public PizzaBuilder setSize(String size){
+            this.size = size;
+            return this;
+        }
+
+        public PizzaBuilder setCrust(String crust){
+            this.crust = crust;
+            return this;
+        }
+
+        public PizzaBuilder addTopping(String topping){
+            this.toppings.add(topping);
+            return this;
+        }
+
+        Pizza build(){
+            return new Pizza(this);
+        }
+    }
+}
